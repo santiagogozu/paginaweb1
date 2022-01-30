1e8847818bdff89ef374dbc9084a960fb1d3eba6
santiagogozu <santiagogo1993@gmail.com>
2022-01-30 17:24:06 -0500

    primer archivo

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const rutasMain =require("./routers/main.js") //Se conecta con el archivo router
 
 app.use(express.static("public")) //busca archivos estaticos (imagenes y CCS, se coloca "public" poeque etsa en la carpeta con ese nombre)
 
-app.listen(3000, ()=>{
-    console.log("Servidor en puerto 3010")
-})
+app.listen(process.env.PORT||3000, function() {
+    console.log('Servidor funcionando');
+});
 
 app.use("/", rutasMain)
